e7d674b14f4d8635df74db1d002071c2a25a9417
Emirhan Yıldız <emirhanyy53@gmail.com>
2026-07-07 15:28:27 +0300

    feat: add CdrStructureDto model for JSON mapping

---
+++ src/main/java/com/turkcell/cdrgenerator1/model/CdrStructureDto.java
@@ -0,0 +1,22 @@
+package com.turkcell.cdrgenerator1.model; // Kendi paket ismine göre düzenle
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CdrStructureDto {
+
+    private Long id;
+    private String name;
+    private Integer serverid;
+    private String description;
+    private String contents;
+    private Boolean islocked;
+    private String servertype;
+
+}
