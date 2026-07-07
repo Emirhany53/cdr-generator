a95665e4e67a60c32ff8a0e6a66be7406afac726
Emirhan Yıldız <emirhanyy53@gmail.com>
2026-07-07 10:32:15 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/turkcell/cdrgenerator1/CdrGenerator1Application.java
@@ -0,0 +1,13 @@
+package com.turkcell.cdrgenerator1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdrGenerator1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdrGenerator1Application.class, args);
+    }
+
+}
